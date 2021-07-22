59610eb18aaaaf00afa539918d7790d3cca1df64
Breno Brandao <breno_brandao12@hotmail.com>
2021-07-21 21:35:46 -0300

    adicionado arquivos iniciais

--- index.php
+++ index.php
@@ -5,9 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-image: url(https://images.unsplash.com/photo-1528082992860-d520150d6f6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80);
+        }
+
+        form{
+            height: 150px;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            background-color: #bbb;
+            border-radius: 18px;
+        }
+    </style>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="soma.php" method="post">
     <input type="number" name=" num1">
     <input type="number" name=" num2">
     <button type="submit">somar</button>
